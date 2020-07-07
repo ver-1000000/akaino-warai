8e968b23c021c648ff766ed1e425f83f6aed479e
AKAI <sasaki.ver1000000@gmail.com>
2020-07-08 03:57:02 +0900

    feat: implements game core logic

--- scripts.js
+++ scripts.js
@@ -1,18 +1,115 @@
-let DEBUG = {};
+const random = (range = 100) => Math.round(Math.random() * range);
+
+const QUERY_NAME_MAP = {
+  '.body': '身体―― Body',
+  '.nose': '鼻―― Nose',
+  '.fang .left': '左のキバ―― Left Fang',
+  '.fang .right': '右のキバ―― Right Fang',
+  '.eye .left': '左目―― Left Eye',
+  '.eye .right': '右目―― Right Eye',
+};
+
+class Entity {
+  uniqQuery;
+  name;
+  element;
+  animationFrame = null;
+
+  set fill(value) {
+    this.element.setAttribute('fill', value);
+  }
+
+  set stroke(value) {
+    this.element.setAttribute('stroke', value);
+  }
+
+  set transform(value) {
+    this.element.setAttribute('transform', value);
+  }
+
+  set transformOrigin(value) {
+    this.element.setAttribute('transform-origin', value);
+  }
+
+  constructor(uniqQuery) {
+    this.uniqQuery = uniqQuery;
+    this.name      = QUERY_NAME_MAP[uniqQuery] || '';
+    this.element   = document.querySelector(uniqQuery);
+  }
+}
+
+class Service {
+  frame       = new Entity('.frame');
+  topText     = new Entity('.top-text');
+  bgColor     = '';
+  accentColor = '';
+
+  constructor() {
+    const hue           = random(360);
+    this.bgColor        = `hsl(${hue}, 100%, 70%)`;
+    this.accentColor    = `hsl(${(hue - 90) % 360}, 100%, 40%)`;
+    this.topText.stroke = this.accentColor;
+    this.frame.element.classList.remove('loading');
+    this.frame.element.style.setProperty('background-color', this.bgColor);
+    this.updateTopText('クリックすると始まるよ！―― Click to Start!');
+  }
+
+  updateTopText(text = '') {
+    this.topText.element.innerHTML = text;
+  }
+}
+
+class Game {
+  animEntities  = Object.keys(QUERY_NAME_MAP).map(x => new Entity(x));
+  currentEntity = null;
+  service;
+
+  constructor(service) {
+    this.service = service;
+    this.start();
+  }
+
+  start() {
+    this.service.frame.element.addEventListener('click', () => {
+      if (this.currentEntity == null) {
+        this.animEntities.forEach(entity => this.animate(entity));
+        this.currentEntity = this.animEntities[0];
+      } else {
+        cancelAnimationFrame(this.currentEntity.animationFrame);
+        this.currentEntity.animationFrame = null;
+        this.currentEntity.stroke         = null;
+        this.currentEntity                = this.animEntities[this.animEntities.indexOf(this.currentEntity) + 1];
+      }
+      if (this.currentEntity) {
+        this.service.updateTopText(this.currentEntity.name);
+        this.currentEntity.stroke = this.service.accentColor;
+      } else {
+        this.service.updateTopText();
+      }
+    });
+  }
+
+  animate(entity) {
+    const update = ({ origin, x, y, hue, scale, rotate, stop })=> {
+      const dOrigin          = 50 + Math.abs(5 - origin);
+      const dScale           = 0.5 + Math.abs(scale);
+      const dX               = 64 - Math.abs(128 - x);
+      const dY               = 64 - Math.abs(128 - x);
+      entity.transform       = `translate(${dX} ${dY}) scale(${dScale}) rotate(${rotate})`;
+      entity.transformOrigin = `${dOrigin}px ${40}px`;
+      origin                 = (origin + 1) % 10;
+      x                      = (x + 7) % 256;
+      y                      = (y + 3) % 256;
+      hue                    = (hue + 7) % 360;
+      scale                  = ((scale + 5) * 10 + 1) % 100 / 10 - 5;
+      rotate                 = (rotate + 9) % 360;
+      entity.animationFrame  = requestAnimationFrame(() => update({ origin, x, y, hue, scale, rotate }));
+    };
+    update({ origin: random(10), x: random(128), y: random(128), hue: random(128), scale: random(5), rotate: random(360) });
+  };
+};
 
 addEventListener('load', async load => {
-  const dom       = (query) => document.querySelector(query);
-  const frame     = dom('.frame');
-  const body      = dom('.body');
-  const nose      = dom('.nose');
-  const fangLeft  = dom('.fang .left');
-  const fangRight = dom('.fang .right');
-  const eyeLeft   = dom('.eye .left');
-  const eyeRight  = dom('.eye .right');
-  const topText   = dom('.top-text');
-  DEBUG = { body, nose, fangLeft, fangRight, eyeLeft, eyeRight, topText };
-
-  frame.onclick = e => {
-    topText.innerHTML = 'Clicked';
-  };
+  const service = new Service();
+  new Game(service);
 });
