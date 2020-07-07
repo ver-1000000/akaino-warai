4ee266f4f20a64266e93b13b47c9ccbdae7a033c
AKAI <sasaki.ver1000000@gmail.com>
2020-07-08 06:16:24 +0900

    feat: implements reset button

--- scripts.js
+++ scripts.js
@@ -36,26 +36,59 @@ class Entity {
     this.name      = QUERY_NAME_MAP[uniqQuery] || '';
     this.element   = document.querySelector(uniqQuery);
   }
+
+  classRemove(...tokens) {
+    this.element.classList.remove(tokens);
+  }
+
+  classAdd(...tokens) {
+    this.element.classList.add(tokens);
+  }
 }
 
 class Service {
-  frame       = new Entity('.frame');
-  topText     = new Entity('.top-text');
-  bgColor     = '';
-  accentColor = '';
+  bgColor         = '';
+  accentColor     = '';
+  world;
+  topText;
+  saveImageButton;
+  tweetButton;
+  resetButton;
+  cloneWorldNode;
 
   constructor() {
-    const hue           = random(360);
-    this.bgColor        = `hsl(${hue}, 100%, 70%)`;
-    this.accentColor    = `hsl(${(hue - 90) % 360}, 100%, 40%)`;
-    this.topText.stroke = this.accentColor;
-    this.frame.element.classList.remove('loading');
-    this.frame.element.style.setProperty('background-color', this.bgColor);
-    this.updateTopText('クリックすると始まるよ！―― Click to Start!');
+    this.reset();
   }
 
   updateTopText(text = '') {
     this.topText.element.innerHTML = text;
+  }
+
+  appearButtons() {
+    const buttons = new Entity('.buttons');
+    buttons.classRemove('hidden');
+  }
+
+  reset() {
+    if (this.cloneWorldNode) {
+      this.world.element.parentNode.replaceChild(this.cloneWorldNode, this.world.element);
+    }
+    this.world           = new Entity('.world');
+    this.topText         = new Entity('.top .text');
+    this.saveImageButton = new Entity('.bottom .left.button');
+    this.tweetButton     = new Entity('.bottom .center.button');
+    this.resetButton     = new Entity('.bottom .right.button');
+    this.cloneWorldNode  = this.world.element.cloneNode(true);
+
+    const hue            = random(360);
+    this.bgColor         = `hsl(${hue}, 100%, 70%)`;
+    this.accentColor     = `hsl(${(hue - 90) % 360}, 100%, 40%)`;
+    this.topText.stroke  = this.accentColor;
+
+    this.world.classRemove('loading');
+    this.world.element.style.setProperty('background-color', this.bgColor);
+    this.updateTopText('クリックすると始まるよ！―― Click to Start!');
+    new Game(this);
   }
 }
 
@@ -70,27 +103,45 @@ class Game {
   }
 
   start() {
-    this.service.frame.element.addEventListener('click', () => {
+    const resetButtonClick = e => {
+      e.stopPropagation();
+      this.service.reset();
+      this.service.resetButton.element.removeEventListener('click', resetButtonClick);
+    };
+    const worldClick = () => {
       if (this.currentEntity == null) {
+        // 初回
         this.animEntities.forEach(entity => this.animate(entity));
         this.currentEntity = this.animEntities[0];
       } else {
+        // 初回以降
         cancelAnimationFrame(this.currentEntity.animationFrame);
         this.currentEntity.animationFrame = null;
         this.currentEntity.stroke         = null;
         this.currentEntity                = this.animEntities[this.animEntities.indexOf(this.currentEntity) + 1];
       }
       if (this.currentEntity) {
+        // 終了以前
         this.service.updateTopText(this.currentEntity.name);
         this.currentEntity.stroke = this.service.accentColor;
       } else {
-        this.service.updateTopText();
+        // 終了処理
+        this.finish();
+        this.service.world.element.removeEventListener('click', worldClick);
       }
-    });
+    };
+    this.service.resetButton.element.addEventListener('click', resetButtonClick);
+    this.service.world.element.addEventListener('click', worldClick);
+  }
+
+  finish() {
+    this.service.world.classAdd('loading');
+    this.service.updateTopText('完成！―― Finish!');
+    this.service.appearButtons();
   }
 
   animate(entity) {
-    const update = ({ origin, x, y, hue, scale, rotate, stop })=> {
+    const update = ({ origin, x, y, scale, rotate, stop })=> {
       const dOrigin          = 50 + Math.abs(5 - origin);
       const dScale           = 0.5 + Math.abs(scale);
       const dX               = 64 - Math.abs(128 - x);
@@ -100,16 +151,14 @@ class Game {
       origin                 = (origin + 1) % 10;
       x                      = (x + 7) % 256;
       y                      = (y + 3) % 256;
-      hue                    = (hue + 7) % 360;
       scale                  = ((scale + 5) * 10 + 1) % 100 / 10 - 5;
       rotate                 = (rotate + 9) % 360;
-      entity.animationFrame  = requestAnimationFrame(() => update({ origin, x, y, hue, scale, rotate }));
+      entity.animationFrame  = requestAnimationFrame(() => update({ origin, x, y, scale, rotate }));
     };
-    update({ origin: random(10), x: random(128), y: random(128), hue: random(128), scale: random(5), rotate: random(360) });
+    update({ origin: random(10), x: random(128), y: random(128), scale: random(5), rotate: random(360) });
   };
 };
 
 addEventListener('load', async load => {
   const service = new Service();
-  new Game(service);
 });
